df7b64eceecead97e90d948cf1c5edc9cbf7ebe1
Takeshi MUTOH <takeshi.mutoh@gmail.com>
2016-07-13 10:19:37 +0900

    Add LED input support.

--- demo/RetroBSD/led.sh
+++ demo/RetroBSD/led.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 LED_PORT=/dev/porte
+ADC_PITCH=50
+
 # Init port
 echo ........iiiioooo > /dev/confe
 c=0
@@ -7,6 +9,25 @@ out=0000
 header=............
 while true
 do
+    # ADC
+    dev=0
+    while [ ${dev} -lt 4 ]
+    do
+        adc=`cat /dev/adc${dev}|head -1`
+        echo -n "ADC\t${dev}:\t"${adc}"\t"
+        adc_div=`expr ${adc} / ${ADC_PITCH}`
+        i=0
+        while [ ${i} -lt ${adc_div} ]
+        do
+            echo -n "="
+            i=`expr ${i} + 1`
+        done
+        echo
+        dev=`expr $dev + 1`
+    done
+    echo "---"
+
+    # GPIO LED
     if [ `expr ${c} % 4` -eq 0 ]; then
       out=`echo ${out}|sed 's/^.//'|sed 's/$/1/'`
     else
