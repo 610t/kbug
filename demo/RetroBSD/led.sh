536d38cad0196c1022b0be6d96f3f42b82c72344
Takeshi MUTOH <takeshi.mutoh@gmail.com>
2016-07-25 10:12:13 +0900

    Create blink function for blinking all LEDs. Create LED switch input logic.

--- demo/RetroBSD/led.sh
+++ demo/RetroBSD/led.sh
@@ -1,26 +1,37 @@
 #!/bin/sh
 LED_PORT=/dev/porte
+SW_PORT=/dev/portd
 ADC_PITCH=50
+
+# Blink all LEDs 8 times.
+blink () {
+  l=0
+  while [ ${l} -lt 8 ]
+  do
+    echo ........11111111 > /dev/porte
+#    sleep 1
+    echo ........00000000 > /dev/porte
+#    sleep 1
+    l=`expr ${l} + 1`
+  done
+}
 
 # Init port
 echo ........oooooooo > /dev/confe
+echo ..............i. > /dev/confd
 c=0
 out=00000000
 header=........
-# Blink
-l=0
-while [ ${l} -lt 8 ]
-do
-  echo ........11111111 > /dev/porte
-  echo ........00000000 > /dev/porte
-  l=`expr ${l} + 1`
-done
+
+blink
 
 while true
 do
     # ADC
     dev=0
-    while [ ${dev} -lt 5 ]
+    # Currently skip adc input.
+    while [ ${dev} -lt 0 ]
+#    while [ ${dev} -lt 5 ]
     do
         adc=`cat /dev/adc${dev}|head -1`
         echo -n "ADC\t${dev}:\t"${adc}"\t"
@@ -37,13 +48,24 @@ do
     echo "---"
 
     # GPIO LED
-    bnum=`echo "2 o ${c} p"|dc`
-    out=`printf "%08d" ${bnum}`
+#    echo "2 o ${c} p" | dc
+    if [ `expr ${c} % 8` -eq 0 ]; then
+      out=`echo ${out}|sed 's/^.//'|sed 's/$/1/'`
+    else
+      out=`echo ${out}|sed 's/^.//'|sed 's/$/0/'`
+    fi
     echo ${header}${out} > ${LED_PORT}
     echo ${out}
     in=`cat ${LED_PORT}`
     sw=`echo ${in}|sed 's/^........//'|sed 's/....$//'`
     echo ${in}:${sw}
     c=`expr ${c} + 1`
-    echo ${c}" "`date`
+#    echo ${c}" "`date`
+
+    # GPIO SW
+    sw=`cat ${SW_PORT}|sed -e 's/^-*\([01]\)-$/\1/'`
+    echo "SW:"${sw}
+    if [ ${sw} -eq 1 ]; then
+      blink
+    fi
 done
