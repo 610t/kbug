fe270f9cf40c7b01d02b14bdbb31d74fe7a06332
mutoh <takeshi.mutoh@gmail.com>
2017-03-03 08:04:30 +0900

    Button event is changed. Add string display function.

--- demo/microbit/sensor.py
+++ demo/microbit/sensor.py
@@ -2,9 +2,14 @@ from microbit import *
 
 def get_sensor_data():
     x, y, z = accelerometer.get_x(), accelerometer.get_y(), accelerometer.get_z()
-    a, b = button_a.was_pressed(), button_b.was_pressed()
+    a, b = button_a.is_pressed(), button_b.is_pressed()
     print(x, y, z, a, b)
+
+uart.init(115200)
 
 while True:
     sleep(100)
     get_sensor_data()
+    if uart.any():
+        str=uart.readline()
+        display.scroll(str,wait=False,loop=True)
