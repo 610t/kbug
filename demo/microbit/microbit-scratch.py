1112eba1d3cccb2f1b17329b844b606647881b65
mutoh <takeshi.mutoh@gmail.com>
2017-03-03 13:31:36 +0900

    Change Scratch handle library to pyscratch.

--- demo/microbit/microbit-scratch.py
+++ demo/microbit/microbit-scratch.py
@@ -1,11 +1,18 @@
+#!/usr/local/bin/python2.7
 import serial
-from time import sleep
 import scratch
 
-scr = scratch.Scratch()
-## THE NEXT LINE MIGHT NEED TO BE CHANGED - TYPE ls /dev/ttyA* into the terminal to see which port is needed.
-PORT = "/dev/ttyACM1"
-## 
+class ScratchReceiver(object):
+  @staticmethod
+  def broadcast_handler(message):
+    print('[receive] broadcast:', message)
+    s.write(message.encode('utf-8'))
+  @staticmethod
+  def sensor_update_handler(**sensor_data):
+    for name, value in sensor_data.items():
+      print('[receive] sensor-update:', name, value)
+
+PORT = "/dev/cuaU0"
 BAUD = 115200
 
 s = serial.Serial(PORT)
@@ -14,18 +21,19 @@ s.parity   = serial.PARITY_NONE
 s.databits = serial.EIGHTBITS
 s.stopbits = serial.STOPBITS_ONE
 
+rsc = scratch.RemoteSensorConnection(ScratchReceiver.broadcast_handler, ScratchReceiver.sensor_update_handler)
+rsc.connect()
 while True:
     data = s.readline().decode('UTF-8')
     data_list = data.rstrip().split(' ')
     try:
         x, y, z, a, b = data_list
-        scr.sensorupdate({'x' : x})
-        scr.sensorupdate({'y' : y})
-        scr.sensorupdate({'z' : z})
-        scr.sensorupdate({'a' : a})
-        scr.sensorupdate({'b' : b})
-
+        print(x,y,z,a,b)
+        rsc.send_sensor_update(x=int(x))
+        rsc.send_sensor_update(y=int(y))
+        rsc.send_sensor_update(z=int(z))
+        rsc.send_sensor_update(a=str(a))
+        rsc.send_sensor_update(b=str(b))
     except:
         pass
-
-s.close()
+rsc.disconnect()
